424ee3e38ed3e3b51f67647115c525c650223596
HighSoft <highsoft85@outlook.com>
2024-06-10 17:10:04 +0200

    Summaries changed

--- app/lib/data/educations.js
+++ app/lib/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Computer Applications, Computer science",
+    duration: "2015 - 2019",
+    institution: "Megatrend Univerzitet",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Computer Technician Certificate",
+    duration: "2012 - 2015",
+    institution: "Technical School",
   }
 ]
--- app/lib/data/personal.js
+++ app/lib/data/personal.js
@@ -4,7 +4,6 @@ export const personalData = {
   devUsername: "highsoft85",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My nickname is Highsoft85. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more convenient and functional. I have so much experiences in frontend and backend stacks. I am available for any kind of job opportunity that suits my skills and interests. And you would be satisfied with my result.",
   email: 'highsoft85@outlook.com',
   phone: '+8801608797655',
   address: 'Kneza Mihaila 23b Obrenovac, SE 11500 Serbia',
@@ -15,5 +14,6 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/24877703/highsoft85',
   devTo: 'https://dev.to/highsoft85',
   leetcode: "https://leetcode.com/highsoft85/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  description: "I am a professional and enthusiastic full-stack developer. As a freelance full-stack developer, I bring a robust skill set and years of experience to the table. And I am a quick learner with a self-learning attitude. I love to explore new technologies and am passionate about problem-solving. I love to make the web more convenient and functional. I am available for any kind of job opportunity that suits my skills and interests. And you would be satisfied with my result.",
 }
