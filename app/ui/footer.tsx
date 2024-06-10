6872a29de4c5467a886fcbee6c6957c97ad9849a
HighSoft <highsoft85@outlook.com>
2024-06-10 16:05:02 +0200

    Minior changes

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -20,7 +20,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/highsoft85/nextjs-dev-portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -28,7 +28,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://github.com/highsoft85/nextjs-dev-portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
