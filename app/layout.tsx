8cfac0c09b45fc3969f5338106853e479222a6c5
HighSoft <highsoft85@outlook.com>
2024-06-19 16:47:09 +0200

    Moved GTM noscript section

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <noscript>
+          <iframe 
+            src={`https://www.googletagmanager.com/ns.html?id={process.env.NEXT_PUBLIC_GTM}`}
+            height="0" width="0" 
+            style={{display:"none", visibility:"hidden"}}>
+          </iframe>
+        </noscript>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">          
           <Navbar />
@@ -34,14 +41,6 @@ export default function RootLayout({
         <Footer />
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM || ""} />
-
-      <noscript>
-        <iframe 
-          src={`https://www.googletagmanager.com/ns.html?id={process.env.NEXT_PUBLIC_GTM}`}
-          height="0" width="0" 
-          style={{display:"none", visibility:"hidden"}}>
-        </iframe>
-      </noscript>
 </html>
   );
 }
