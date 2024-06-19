8dcde6a808a3b6d16862d73070da750dc1d695af
HighSoft <highsoft85@outlook.com>
2024-06-19 16:36:41 +0200

    Added GTM script

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,14 @@ export default function RootLayout({
         <Footer />
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM || ""} />
-    </html>
+
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PGCVJGZ6"
+          height="0" width="0" 
+          style={{display:"none", visibility:"hidden"}}>
+        </iframe>
+      </noscript>
+</html>
   );
 }
